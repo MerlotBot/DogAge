029875b3ba7d309e0931d5e5095e27f45aa50df3
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-05-10 21:09:15 +0200

    DogAge complete

--- DogAge/ViewController.swift
+++ DogAge/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dogAgeTextField: UITextField!
+    @IBOutlet weak var humanAgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        
+        if dogAgeTextField.text != "" {
+        let dogAge = Int(dogAgeTextField.text!)!
+            humanAgeLabel.text = "In human years: \(calculateHumanAge(dogAge: dogAge))"
+        } else {
+            humanAgeLabel.text = "Please input dog age!"
+        }
+    }
+    
+    
+    func calculateHumanAge(dogAge: Int) -> Int {
+        
+        var humanAge: Int = 0
+        
+        if dogAge == 0 {
+            
+        } else if dogAge == 1 {
+            humanAge = 15
+        } else if dogAge == 2 {
+            humanAge = 24
+        } else {
+            humanAge = ((dogAge - 2) * 4) + 24
+        }
+        
+        return humanAge
+    }
 }
 
